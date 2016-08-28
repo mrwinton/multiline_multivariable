0efd0bd58562c4d018fb1b3b3f3f56f4056504ea
Michael Winton <wintonmr@gmail.com>
2016-08-28 16:34:26 +1200

    Added static navigator.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var Chart = (function(window, d3, self) {
-  var svg, data, x, y, xAxis, yAxis, dim, chartWrapper, line, tagPaths, margin = {}, width, height;
+  var svg, data, x, y, xAxis, yAxis, dim, chart, area, clip, line, tagPaths, margin = {}, width, height;
   var breakPoint = 320;
+
+  var navWidth, navHeight, navChart, navX, navY, navXAxis, navLine, navSvg, navChart, navTagPaths;
 
   var tagTemperatureReadings = [];
   var tagRelativeHumidityReadings = [];
   var tagDewPointReadings = [];
   var tagEquilibriumMoistureContentReadings = [];
 
-  var yTemperature = null;
-  var yRelativeHumidity = null;
-  var yDewPoint = null;
-  var yEquilibriumMoistureContent = null;
+  var yTemperature, yRelativeHumidity, yDewPoint, yEquilibriumMoistureContent;
+  var navYTemperature, navYRelativeHumidity, navYDewPoint, navYEquilibriumMoistureContent;
 
   var timeFormat = d3.time.format('%Y-%m-%d %H:%M:%S');
 
@@ -52,8 +52,8 @@ var Chart = (function(window, d3, self) {
 
     //initialize scales
     var xExtent = d3.extent(tagLogsArray, function(d,i) { return timeFormat.parse(d.readingAt) });
-    // var xExtent = d3.extent(tagLogsArray, function(d,i) { return new Date(d.readingAt) });
     x = d3.time.scale().domain(xExtent);
+    navX = d3.time.scale().domain(xExtent);
 
     var yTemperatureExtent = d3.extent(tagLogsArray, function(d,i) { return d.temperature });
     var yRelativeHumidityExtent = d3.extent(tagLogsArray, function(d,i) { return d.relativeHumidity });
@@ -64,32 +64,59 @@ var Chart = (function(window, d3, self) {
     yRelativeHumidity = d3.scale.linear().domain(yRelativeHumidityExtent);
     yDewPoint = d3.scale.linear().domain(yDewPointExtent);
     yEquilibriumMoistureContent = d3.scale.linear().domain(yEquilibriumMoistureContentExtent);
+
+    navYTemperature = d3.scale.linear().domain(yTemperatureExtent);
+    navYRelativeHumidity = d3.scale.linear().domain(yRelativeHumidityExtent);
+    navYDewPoint = d3.scale.linear().domain(yDewPointExtent);
+    navYEquilibriumMoistureContent = d3.scale.linear().domain(yEquilibriumMoistureContentExtent);
   }
 
   function initChart(){
     //initialize axis
     xAxis = d3.svg.axis().orient('bottom');
     yAxis = d3.svg.axis().orient('left');
+    navXAxis = d3.svg.axis().orient('bottom');
 
     //initialize y scale as temperature
     y = yTemperature;
+    navY = navYTemperature;
 
     //the path generator for the line chart
     line = d3.svg.line()
       .x(function(d) { return x(timeFormat.parse(d.date)) })
       .y(function(d) { return y(d.y) });
 
+    navLine = d3.svg.line()
+      .x(function (d) { return navX(timeFormat.parse(d.date)) })
+      .y(function (d) { return navY(d.y) });
+
     //initialize svg with temperature readings
     svg = d3.select('#chart').append('svg');
-    chartWrapper = svg.append('g');
-    tagPaths = chartWrapper.selectAll(".tag")
+    chart = svg.append('g');
+    area = chart.append('g').attr('clip-path', 'url(#plotAreaClip)');
+    clip = area.append('clipPath')
+        .attr('id', 'plotAreaClip')
+        .append('rect');
+
+    chart.append('g').classed('x axis', true);
+    chart.append('g').classed('y axis', true);
+
+    tagPaths = chart.selectAll(".tag")
         .data(tagTemperatureReadings)
         .enter().append("g")
         .attr("class", "tag")
         .append("path");
 
-    chartWrapper.append('g').classed('x axis', true);
-    chartWrapper.append('g').classed('y axis', true);
+    navSvg = d3.select('#chart').append('svg')
+      .classed('navigator', true);
+    navChart = navSvg.append('g');
+    navChart.append('g').classed('x axis', true);
+
+    navTagPaths = navChart.selectAll(".tag")
+        .data(tagTemperatureReadings)
+        .enter().append("g")
+        .attr("class", "tag")
+        .append("path");
   }
 
   function render(event) {
@@ -100,42 +127,51 @@ var Chart = (function(window, d3, self) {
     //update x and y scales to new dimensions
     x.range([0, width]);
     y.range([height, 0]);
+    navX.range([0, navWidth]);
+    navY.range([navHeight, 0]);
 
     //update svg elements to new dimensions
-    svg.attr('width', width + margin.right + margin.left)
+    svg.attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom);
-    chartWrapper.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    chart.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    clip.attr({ width: width, height: height });
 
-    if(newWidth < breakPoint) {
-      xAxis.ticks(d3.time.month, 2)
-    }
-    else {
-      xAxis.ticks(d3.time.month, 1)
-    }
+    navSvg.attr('width', navWidth + margin.left + margin.right)
+      .attr('height', navHeight + margin.top + margin.bottom);
+    navChart.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    xAxis.ticks(5);
+    navXAxis.ticks(5);
 
     var xAxisElement = svg.select('.x.axis');
     var yAxisElement = svg.select('.y.axis');
     var localTagPaths = tagPaths.attr("class", "line");
+    var navXAxisElement = navSvg.select('.x.axis');
+    var localNavTagPaths = navTagPaths.attr("class", "line");
 
     if(event == null){
-      xAxisElement = xAxisElement.transition().duration(1000)
-      yAxisElement = yAxisElement.transition().duration(1000)
-      localTagPaths = localTagPaths.transition().duration(1000)
+      xAxisElement = xAxisElement.transition().duration(1000);
+      yAxisElement = yAxisElement.transition().duration(1000);
+      localTagPaths = localTagPaths.transition().duration(1000);
+      navXAxisElement = navXAxisElement.transition().duration(1000);
+      localNavTagPaths = localNavTagPaths.transition().duration(1000);
     }
 
     //update the axis and line
     xAxis.scale(x);
     yAxis.scale(y).orient(newWidth < breakPoint ? 'right' : 'left');
+    navXAxis.scale(navX);
 
-    xAxisElement
-      .attr('transform', 'translate(0,' + height + ')')
+    xAxisElement.attr('transform', 'translate(0,' + height + ')')
       .call(xAxis);
 
-    yAxisElement
-      .call(yAxis);
+    yAxisElement.call(yAxis);
 
-    localTagPaths
-      .attr("d", function(d) { return line(d.values); });
+    navXAxisElement.attr('transform', 'translate(0,' + navHeight + ')')
+      .call(navXAxis);
+
+    localTagPaths.attr("d", function(d) { return line(d.values); });
+    localNavTagPaths.attr("d", function(d) { return navLine(d.values); });
   }
 
   function updateDimensions(newWidth) {
@@ -146,29 +182,40 @@ var Chart = (function(window, d3, self) {
 
     width = newWidth - margin.left - margin.right;
     height = .55 * width;
+
+    navWidth = width;
+    navHeight = 100 - margin.top - margin.bottom;
   }
 
   function renderData(dataType) {
     if(dataType === "temperature"){
       // update tagPaths
       tagPaths.data(tagTemperatureReadings);
+      navTagPaths.data(tagTemperatureReadings);
       // update yScale
       y = yTemperature;
+      navY = navYTemperature;
     } else if (dataType === "relativeHumidity"){
       // update tagPaths
       tagPaths.data(tagRelativeHumidityReadings);
+      navTagPaths.data(tagRelativeHumidityReadings);
       // update yScale
       y = yRelativeHumidity;
+      navY = navYRelativeHumidity;
     } else if (dataType === "dewPoint") {
       // update tagPaths
       tagPaths.data(tagDewPointReadings);
+      navTagPaths.data(tagDewPointReadings);
       // update yScale
       y = yDewPoint;
+      navY = navYDewPoint;
     } else if (dataType === "equilibriumMoistureContent") {
       // update tagPaths
       tagPaths.data(tagEquilibriumMoistureContentReadings);
+      navTagPaths.data(tagEquilibriumMoistureContentReadings);
       // update yScale
       y = yEquilibriumMoistureContent;
+      navY = navYEquilibriumMoistureContent;
     } else {
       console.log(dataType + " not recognised.");
       return;
